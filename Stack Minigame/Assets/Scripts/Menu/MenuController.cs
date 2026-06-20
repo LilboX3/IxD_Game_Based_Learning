702c68de42808e7efd7ed422264dd19c33c5dcdb
LilboX3 <lilligruber02@gmail.com>
2026-06-20 10:59:17 +0200

    Ádded main menu

--- Stack Minigame/Assets/Scripts/Menu/MenuController.cs
+++ Stack Minigame/Assets/Scripts/Menu/MenuController.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadNextScene()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnApplicationQuit()
     {
-        
+        Application.Quit();
     }
 }
