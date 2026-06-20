c96829ba84960cdae35d5499320742994a357730
LilboX3 <lilligruber02@gmail.com>
2026-06-20 15:55:16 +0200

    fixed AI panel not opening

--- Stack Minigame/Assets/Scripts/Hub/AIController.cs
+++ Stack Minigame/Assets/Scripts/Hub/AIController.cs
@@ -8,40 +8,44 @@ public class AIController : MonoBehaviour
     [SerializeField] private TextMeshPro aiText;
     [SerializeField] private GameObject chatPanel;
 
-    private bool _showingAiText = false;
+    [SerializeField] private bool _showingAiText = false;
 
     public bool isAiOpen = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        aiText.gameObject.SetActive(_showingAiText);
+        chatPanel.SetActive(isAiOpen);
     }
 
     // Update is called once per frame
     void Update()
     {
-        aiText.gameObject.SetActive(_showingAiText);
-        chatPanel.SetActive(isAiOpen);
+
     }
 
     public void OpenAIWindow()
     {
+        chatPanel.SetActive(true);
         isAiOpen = true;
     }
 
     public void CloseAIWindow()
     {
+        chatPanel.SetActive(false);
         isAiOpen = false;
     }
 
     public void ShowText()
     {
+        aiText.gameObject.SetActive(true);
         _showingAiText = true;
     }
 
     public void HideText()
     {
+        aiText.gameObject.SetActive(false);
         _showingAiText = false;
     }
 }
